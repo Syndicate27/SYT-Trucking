be9260848c45357d9d19a230aa4bfffa29bad8e2
Jeremiah Nji <jeremiah.syndicate@gmail.com>
2016-12-01 15:44:28 -0800

    Php form
    All variables have been created on the form

---
+++ SYT-Trucking-Homepage/SYT_Info.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PHP SYT Contact Form</title>
+</head>
+
+<body>
+	<?php
+		// Variables for information entered in on the application
+		$company = $_POST["company"]; //Comapny Name
+		$month = $_POST["month"]; //Month Name
+		$day = $_POST["day"]; //day
+		$year = $_POST["year"]; //Year
+		$p_location = $_POST["PickUpLocation"]; //Pick up location of cargo
+		$d_location = $_POST["DropOffLocation"]; //Drop off location of cargo
+		$tnum = $_POST["ticketNumber"]; //Ticket Number
+		$weight = $_POST["tonage"]; //Weight of total cargo delivered
+		
+		
+		
+		
+	
+	
+	
+	?>
+
+
+</body>
+</html>
